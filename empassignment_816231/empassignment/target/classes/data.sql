7e7df15c0a41692c8bb0f2dd6f1062b23225674f
YuvrajThakur05 <100115014+YuvrajThakur05@users.noreply.github.com>
2022-03-04 15:38:03 +0530

    Add files via upload

---
+++ empassignment_816231/empassignment/target/classes/data.sql
@@ -0,0 +1,25 @@
+insert into employee(id, name, last_name) 
+values(100,'akshay','sarda');
+insert into employee (id, name, last_name) 
+values(101,'krishna' ,'kittu' );
+insert into employee(id, name, last_name ) 
+values(102,'ram', 'chandhra');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
